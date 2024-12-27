c158598664baff7be96dccabb2fe0e7753ae50ba
Morgany Vieira Mendes <morgany@mangustotech.com>
2024-12-26 22:26:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setuptools.setup(
          'Programming Language :: Python :: 2.7',
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.6',
-         'Programming Language :: Python :: 3.8'
+         'Programming Language :: Python :: 3.8',
+         'Programming Language :: Python :: 3.10'
     ],
     packages=setuptools.find_packages(exclude=['tests*']),
     include_package_data=True,
