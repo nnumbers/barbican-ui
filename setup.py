7c6068ed144200ede1a4d2575154f55397475a9c
Morgany Vieira Mendes <morgany@mangustotech.com>
2025-01-03 16:04:29 -0300

    version upgrade to 0.1.22

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError:
 
 setuptools.setup(
     name='barbican-ui',
-    version='0.1.21',
+    version='0.1.22',
     summary = 'Barbican User Interface',
     description_file ='README.rst',
     author='OpenStack',
